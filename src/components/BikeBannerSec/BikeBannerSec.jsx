afcd639143ade14371f7e529795cc4ed30543f27
Danilo P. de Souza <121466843+DevDaniloSouza@users.noreply.github.com>
2023-09-22 12:10:56 -0300

    fat bob page atualizada

--- src/components/BikeBannerSec/BikeBannerSec.jsx
+++ src/components/BikeBannerSec/BikeBannerSec.jsx
@@ -2,9 +2,8 @@ import "./BikeBannerSec.css"
 
 function BikeBannerSec(props) {
     return (
-        <section>
+        <section className="bike-banner" style={{backgroundImage:"url(" + props.imagem + ")"}}>
             <h1 className="motorcycle">Fat Bob 114™</h1>
-            <img src={props.image} alt="Imagem Motocicleta" />
         </section>
     )
 }
